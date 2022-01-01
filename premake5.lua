c06deeba319fae76d137565e89614dda23575647
buryakig <buryakigr@gmail.com>
2022-01-01 19:29:33 +0200

    Added a premake file and bat script for generating VS solution files

--- premake5.lua
+++ premake5.lua
@@ -8,21 +8,98 @@ workspace "BlinkEngine"
 		"Dist"
 	}
 
-outputdir = "%{cfg.build}-%{cfg.system}-%{cfg.architecture}"
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 project "BlinkEngine"
 	location"BlinkEngine"
 	kind "SharedLib"
 	language "C++"
 
-	targetdir("bin/" .. outputdir .. "/%{prj.name}")
-	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir("bin/build/" .. outputdir .. "/%{prj.name}")
+	objdir("bin/int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
-		"%{prj.name}/src/**.h"
+		"%{prj.name}/src/**.h",
 		"%{prj.name}/src/**.cpp"
 	}
 
-	BLINK_PLATFORM_WINDOWS;BLINK_BUILD_DLL;
-	
+	includedirs
+	{
+		"%{prj.name}/submodules/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"BLINK_PLATFORM_WINDOWS",
+			"BLINK_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/build/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "BLINK_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "BLINK_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "BLINK_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location"Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir("bin/build/" .. outputdir .. "/%{prj.name}")
+	objdir("bin/int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"BlinkEngine/submodules/spdlog/include",
+		"BlinkEngine/src"
+	}
+
+	links
+	{
+		"BlinkEngine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"BLINK_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "BLINK_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "BLINK_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "BLINK_DIST"
+		optimize "On"
